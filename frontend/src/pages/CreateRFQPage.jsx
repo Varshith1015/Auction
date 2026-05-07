c40b67df4ac6233d64334451d5cd5d0dc08cbe7d
varshith <varshithravipati2003@gmail.com>
2026-05-07 20:01:19 +0530

    added RFQ creation form fields

--- frontend/src/pages/CreateRFQPage.jsx
+++ frontend/src/pages/CreateRFQPage.jsx
@@ -2,20 +2,67 @@ function CreateRFQPage() {
   return (
     <div>
       <h2>Create RFQ Page</h2>
+
       <form>
         <div>
           <label>RFQ Name</label>
           <br />
-          <input type="text" />
+          <input type="text" name="rfq_name" />
         </div>
+
         <div>
           <label>Reference ID</label>
           <br />
-          <input type="text" />
+          <input type="text" name="reference_id" />
         </div>
-        <button type="submit">
-          Create RFQ
-        </button>
+
+        <div>
+          <label>Bid Start Time</label>
+          <br />
+          <input type="datetime-local" name="bid_start_time" />
+        </div>
+
+        <div>
+          <label>Bid Close Time</label>
+          <br />
+          <input type="datetime-local" name="bid_close_time" />
+        </div>
+
+        <div>
+          <label>Forced Bid Close Time</label>
+          <br />
+          <input type="datetime-local" name="forced_bid_close_time" />
+        </div>
+
+        <div>
+          <label>Pickup / Service Date</label>
+          <br />
+          <input type="date" name="pickup_service_date" />
+        </div>
+
+        <div>
+          <label>Trigger Window Minutes</label>
+          <br />
+          <input type="number" name="trigger_window_minutes" />
+        </div>
+
+        <div>
+          <label>Extension Duration Minutes</label>
+          <br />
+          <input type="number" name="extension_duration_minutes" />
+        </div>
+
+        <div>
+          <label>Extension Trigger Type</label>
+          <br />
+          <select name="extension_trigger_type">
+            <option value="BID_RECEIVED">Bid Received</option>
+            <option value="ANY_RANK_CHANGE">Any Rank Change</option>
+            <option value="L1_CHANGE">L1 Rank Change</option>
+          </select>
+        </div>
+
+        <button type="submit">Create RFQ</button>
       </form>
     </div>
   );
