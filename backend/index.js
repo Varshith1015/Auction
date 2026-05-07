12c9631889569549f4ebfd1ea1c94e06f1fd6db7
varshith <varshithravipati2003@gmail.com>
2026-05-07 16:13:27 +0530

    Initialized frontend setup

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the RFQ Management System API");
+});
+
 app.use("/api/rfq", rfqRoutes); 
 
 app.listen(PORT, () => {
