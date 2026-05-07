899330aea221406be8ff39ea442dbc6f82735f0a
varshith <varshithravipati2003@gmail.com>
2026-05-07 19:57:09 +0530

    added create RFQ form page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import RFQListPage from "./pages/RFQListPage";
+import CreateRFQPage from "./pages/CreateRFQPage";
 
 function App() {
   return (
     <div>
       <h1>Auction RFQ System</h1>
+      <CreateRFQPage />
+      <hr />
       <RFQListPage />
     </div>
   );
--- frontend/src/pages/CreateRFQPage.jsx
+++ frontend/src/pages/CreateRFQPage.jsx
@@ -1,6 +1,24 @@
-function CreateRFQPage(){
-    return (
-        <h2>Create RFQ Page</h2>
-    );
+function CreateRFQPage() {
+  return (
+    <div>
+      <h2>Create RFQ Page</h2>
+      <form>
+        <div>
+          <label>RFQ Name</label>
+          <br />
+          <input type="text" />
+        </div>
+        <div>
+          <label>Reference ID</label>
+          <br />
+          <input type="text" />
+        </div>
+        <button type="submit">
+          Create RFQ
+        </button>
+      </form>
+    </div>
+  );
 }
+
 export default CreateRFQPage;
